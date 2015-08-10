b595f420eb9bf1f0f970610d8c5b7dca086dc5cf
nanotokyo <eanocreation@gmail.com>
2015-08-10 12:35:34 +0000

    create challenge1 message board

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,18 +1,25 @@
 class MessagesController < ApplicationController
   def index
     @messages = Message.all
+    @message = Message.new
   end
   
-  ##ここから記述
+  #ここから記述
   def create
     @message = Message.new(message_params)
-    @message.save
+    if @message.save
     redirect_to root_path , notice: 'メッセージを保存しました。'
+  else
+  #メッセージが保存できなかったとき
+  @messages = Message.all
+  flash.now[:alert] = "メッセージの保存に失敗しました。"
+  render 'index'
   end
+end
 
 private
   def message_params
     params.require(:message).permit(:name,:body)
   end
-  ##ここまで
+  #ここまで
 end
